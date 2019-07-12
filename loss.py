948f876beda2a9bc7d4ccc6682f5975949b6d01b
zhangodie <43837296+zhangodie@users.noreply.github.com>
2019-07-12 14:56:44 +0800

    Add files via upload

---
+++ loss.py
@@ -0,0 +1,55 @@
+from networks import discriminator
+
+import tensorflow as tf
+#-----------------------------------------------------------------------------------
+# 语义损失
+def l1_loss(A, B):
+    return tf.reduce_mean(abs(A-B))
+#-----------------------------------------------------------------------------------
+# 生成器损失
+def g_loss(fake, GAN_type):
+    if GAN_type == 'GAN':
+        loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+                              labels=tf.ones_like(fake), logits=fake))
+
+    if GAN_type == 'WGAN':
+        loss = -tf.reduce_mean(fake)
+
+    if GAN_type == 'LSGAN':
+        loss = tf.reduce_mean((fake-tf.ones_like(fake)**2))
+
+    return loss
+#-----------------------------------------------------------------------------------
+# 判别器损失
+def d_loss(real, fake, real_blur, GAN_type):
+    if GAN_type == 'GAN': 
+        real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+                                   labels=tf.ones_like(real), logits=real))
+        fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+                                   labels=tf.zeros_like(fake), logits=fake))
+        blured_real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+                                   labels=tf.zeros_like(blured_real), logits=blured_real))
+
+    if GAN_type == 'WGAN':
+        real_loss = -tf.reduce_mean(real)
+        fake_loss = tf.reduce_mean(fake)
+        blured_real_loss = tf.reduce_mean(blured_real)
+
+    if GAN_type == 'LSGAN':
+        real_loss = tf.reduce_mean((real-tf.ones_like(real))**2)
+        fake_loss = tf.reduce_mean(fake**2)
+        blured_real_loss = tf.reduce_mean(blured_real**2)
+
+    return real_loss+fake_loss+blured_real_loss
+#-----------------------------------------------------------------------------------
+# 梯度惩罚
+def gradient_penalty(real_img, fake_image, scope='discriminator'):
+    difference = real_img-fake_image
+    alpha = tf.random_uniform(shape=[1,1,1,1], minval=0., maxval=1.)
+    interpolates = real_img+alpha*difference
+
+    gradients = tf.gradients(discriminator(interpolates, reuse=True, scope=scope), [interpolates])[0]
+    slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients),reduction_indices=[1,2,3]))
+
+    return tf.reduce_mean((slopes-1.)**2)
+#-----------------------------------------------------------------------------------
---
+++ utils.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+import numpy as np
+#-----------------------------------------------------------------------------------
+# 图像读取
+def imreader(x_list, y_list, step, size):
+    length = len(x_list)
+
+    line_idx = step%length
+    x_path = x_list[line_idx]
+    y_path = y_list[line_idx]
+
+    x_image = cv2.imread(x_path, 1) 
+    y_image = cv2.imread(y_path, 1)
+
+    x_image = cv2.resize(x_image, (size, size))
+    x_image = x_image/127.5-1.
+    y_image = cv2.resize(y_image, (size, size))
+    y_image = y_image/127.5-1.
+
+    return x_image, y_image
+#-----------------------------------------------------------------------------------
+# 文件目录创建
+def check_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(logdir)
+#-----------------------------------------------------------------------------------
+# 模型存储
+def save(saver, sess, logdir, step):
+    model_name = 'model'
+    checkpoint_path = os.path.join(logdir, model_name)
+
+    check_dir(logdir)
+
+    saver.save(sess, checkpoint_path, global_step = step)
+    print('The checkpoint has been created.')
+#-----------------------------------------------------------------------------------
+# 还原归一化后的图片
+def imrestore(img):
+    img = (img+1.)*127.5
+
+    return img.astype(np.float32)
+#-----------------------------------------------------------------------------------
+# 将训练过程中的结果结合位一张图片
+def imget(x_image, y_image, G_realx_fakey, G_fakey_fakex, G_realy_fakex, G_fakex_fakey):
+    x_image = imrestore(x_image)
+    y_image = imrestore(y_image)
+
+    G_realx_fakey = imrestore(G_realx_fakey[0])
+    G_fakey_fakex = imrestore(G_fakey_fakex[0])
+    G_realy_fakex = imrestore(G_realy_fakex[0])
+    G_fakex_fakey = imrestore(G_fakex_fakey[0])
+
+    row1 = np.concatenate((x_image, G_realx_fakey, G_fakey_fakex), axis=1)
+    row2 = np.concatenate((y_image, G_realy_fakex, G_fakex_fakey), axis=1)
+
+    return np.concatenate((row1, row2), axis=0)
+#-----------------------------------------------------------------------------------
+# 对齐两个域图片数量
+def alignment_data(A, B):
+    if len(A) == len(B):
+        return A, B, Bs
+
+    if len(A) < len(B):
+        return A, sorted(B)[:len(A)], sorted(Bs)[:len(A)]
+
+    return A[:len(B)], B, Bs
+#-----------------------------------------------------------------------------------
+# 获取图片的路径列表
+def make_train_data_list(dataset_path):
+    x_input_images_raw = glob.glob(os.path.join(x_data_path, "*"))
+    y_input_images_raw = glob.glob(os.path.join(y_data_path, "*"))
+    
+    return add_train_list(x_input_images_raw, y_input_images_raw)
+#-----------------------------------------------------------------------------------
+# 增加批次维度
+def expand_dims(img):
+    return np.expand_dims(np.array(img).astype(np.float32), axis=0)
+#-----------------------------------------------------------------------------------
